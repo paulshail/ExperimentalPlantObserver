57cd18557f72e2a2c8bc9a8da740551300d8b940
PAUL SHAIL <W9194835@live.tees.ac.uk>
2023-02-23 15:31:31 +0000

    Db exists on laptop, need to create tables using EF

--- ExperimentalPlantObserver.Models/DataContext/PlantDataContext.cs
+++ ExperimentalPlantObserver.Models/DataContext/PlantDataContext.cs
@@ -1,6 +1,7 @@
 ﻿using ExperimentalPlantObserver.Models.Models;
 using Microsoft.EntityFrameworkCore;
 using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,10 @@ namespace ExperimentalPlantObserver.Models.DataContext
 {
     public class PlantDataContext : DbContext
     {
+
+        private string _connString = "Data Source=RONLAPTOP;Initial Catalog=EPODatabase;Integrated Security=True";
+
+        #region Tables
 
         //Entity framework creation of database
         public DbSet<Cluster> Clusters { get; init; }
@@ -24,20 +29,41 @@ namespace ExperimentalPlantObserver.Models.DataContext
         public DbSet<SensorMeasurement> SensorMeasurements { get; init; }
         public DbSet<SensorType> SensorTypes { get; init; }
 
+
+        #endregion
+        #region ctors
+
         public PlantDataContext(DbContextOptions<PlantDataContext> options) : base(options)
         {
-           
+
         }
-        
+
+        public PlantDataContext()
+        {
+
+        }
+        #endregion
+
+
+        #region Config
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
-            
+
             // Foreign keys definitions
 
 
         }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+           optionsBuilder.UseSqlServer(_connString);
 
         }
+
+        #endregion
+    }
+
 }
