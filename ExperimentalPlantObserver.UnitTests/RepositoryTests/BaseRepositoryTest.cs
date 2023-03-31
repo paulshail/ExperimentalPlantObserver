67630f142924ccc2ee78659bc65c7db2ee96211d
PAUL SHAIL <W9194835@live.tees.ac.uk>
2023-03-31 16:07:51 +0100

    Unit tests began, investigate useinmemorydatabase

---
+++ ExperimentalPlantObserver.UnitTests/RepositoryTests/BaseRepositoryTest.cs
@@ -0,0 +1,26 @@
+﻿using ExperimentalPlantObserver.Models.DataContext;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExperimentalPlantObserver.UnitTests.RepositoryTests
+{
+    public class BaseRepositoryTest
+    {
+
+        protected PlantDataContext DbContext;
+
+        protected PlantDataContext GetPlantDataContext()
+        {
+            var options = new DbContextOptionsBuilder<PlantDataContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+                return new PlantDataContext(options);
+        }
+
+    }
+}
---
+++ ExperimentalPlantObserver.UnitTests/RepositoryTests/ClusterRepositoryTests.cs
@@ -0,0 +1,18 @@
+﻿using ExperimentalPlantObserver.Repository.Implementation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExperimentalPlantObserver.UnitTests.RepositoryTests
+{
+    public class ClusterRepositoryTests : BaseRepositoryTest
+    {
+
+        private ClusterRepository _clusterRepository;
+
+
+
+    }
+}
